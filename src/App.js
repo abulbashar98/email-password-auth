0c555205b9e4c2d2ecc4e47bc512ae9977fb7b67
abulbashar98 <abulbashar98116@gmail.com>
2022-04-11 00:54:43 +0600

    Form fields validation using required at fitst, then usin validated state to return the function if not validated, then using Form text to give instructions, set error in a state and display in UI, Use Rugular expression to Strengthen the passwor and show the required Charectars in ui when does not match using set error state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import app from "./firebase.init";
 const auth = getAuth(app);
 
 function App() {
-
+  const [error, setError] = useState('')
+  const [validated, setValidated] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,8 +23,24 @@ function App() {
   }
 
   const handleFormSubmit = (event) => {
+
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    if (!/(?=.*[!@#$%^&*])/.test(password)) {
+      setError('Please include a special character in password')
+      return;
+    }
+
+    setValidated(true);
+    setError('');;
+
     console.log('form Submitted')
-    event.preventDefault()
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user
@@ -37,19 +54,28 @@ function App() {
 
   return (
     <div className="w-50 mx-auto mt-5">
-      <Form onSubmit={handleFormSubmit}>
+      <h1 className="text-primary">Please Register</h1>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={handleEmailOnBlur} type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailOnBlur} type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={handlePasswordOnBlur} type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordOnBlur} type="password" placeholder="Password" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
         </Form.Group>
+        <p className="text-danger">{error}</p>
         <Button variant="primary" type="submit">
           Submit
         </Button>
